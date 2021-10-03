9dd4fd56c6536488549dd277a1bbeb52e747d4f2
chimesindeed <gratitudeadore@fastmail.com>
2021-10-03 03:01:20 -0400

    Linked List - Node as class

---
+++ algos/ll.js
@@ -0,0 +1,15 @@
+// Singly Linked List
+
+
+// Each element of list will be a Node
+class Node {
+	constructor(value){
+		this.value = value;
+		this.next_value = null
+	}
+}
+
+const node = new Node(4)
+console.log(node)
+console.log(typeof node)	
+
