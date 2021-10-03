73a5a0f8034220e10845b207db3f550c54aba71b
chimesindeed <gratitudeadore@fastmail.com>
2021-10-03 03:12:06 -0400

    function linkedList returns object

--- algos/ll.js
+++ algos/ll.js
@@ -13,3 +13,20 @@ const node = new Node(4)
 console.log(node)
 console.log(typeof node)	
 
+function linkedList(){
+
+	let head = tail = null;
+	let length = 0; 
+	
+	return ({
+	
+		print: function(){
+			console.log(`HEAD: ${head}| TAIL: ${tail}| LENGTH: ${length}|`)
+		}
+	
+	})
+
+}
+
+const ll = linkedList();
+ll.print() 
